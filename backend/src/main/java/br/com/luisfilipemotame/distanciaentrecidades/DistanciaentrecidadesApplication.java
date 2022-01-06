7d33285b45d7d2ff4ea6a187e7646a5526192bfc
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-01-06 11:56:52 -0300

    Initial Struture Project

---
+++ backend/src/main/java/br/com/luisfilipemotame/distanciaentrecidades/DistanciaentrecidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisfilipemotame.distanciaentrecidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanciaentrecidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistanciaentrecidadesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/luisfilipemotame/distanciaentrecidades/DistanciaentrecidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luisfilipemotame.distanciaentrecidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistanciaentrecidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
