68f12dca2f45352b53ddc3ff680f79d4361c881d
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-01-06 17:48:00 -0300

    - Organize script

--- python/main.py
+++ python/main.py
@@ -4,15 +4,12 @@ from services.configs_service import ConfigsService
 from services.features_service import FeaturesService
 
 
-def read_configs():
-    config_service = ConfigsService(os.path.dirname(os.path.abspath(__file__)), "config.json")
-    return config_service.data_config
-
-
 def main():
+    config_service = ConfigsService(os.path.dirname(os.path.abspath(__file__)), "config.json")
     features_service = FeaturesService()
 
-    configs = read_configs()
+    configs = config_service.data_config
+
     table_city = features_service.excel_to_table(configs['excel_city'], configs['workspace'], "city", "municipios")
     table_states = features_service.excel_to_table(configs['excel_states'], configs['workspace'], "state", "estados")
 
