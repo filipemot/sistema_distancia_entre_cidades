23ba993c70eb71c3a19393ed39302ba75830d2be
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-01-11 14:28:18 -0300

    - Rename variables

--- python/main.py
+++ python/main.py
@@ -5,9 +5,9 @@ from services.state_services import StateServices
 
 
 def main():
-    state = StateServices(os.path.dirname(os.path.abspath(__file__)), "config.json")
+    state_services = StateServices(os.path.dirname(os.path.abspath(__file__)), "config.json")
 
-    city = CityServices(os.path.dirname(os.path.abspath(__file__)), "config.json", state)
+    city_services = CityServices(os.path.dirname(os.path.abspath(__file__)), "config.json", state_services)
 
 
 if __name__ == '__main__':
