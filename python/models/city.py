15cd7defa862ae6ae99c0cd494e4c1f5e742ccfe
Luis Filipe Guedes Mota <filipemot@gmail.com>
2022-01-13 12:11:28 -0300

    - Adjust erros mypy - Add Config route

--- python/models/city.py
+++ python/models/city.py
@@ -1,4 +1,4 @@
-import uuid
+import uuid  # type: ignore
 
 
 class City:
--- python/models/distance.py
+++ python/models/distance.py
@@ -1,12 +1,12 @@
-import uuid
+import uuid  # type: ignore
 
 
 class Distance:
 
-    def __init__(self, id_distance: uuid, id_city_origin: int, id_city_destiny: int, minutes: float,
+    def __init__(self, id_city_origin: int, id_city_destiny: int, minutes: float,
                  travel_time: float, miles: float, kilometers: float, time_at: float, walk_time: float,
                  truck_time: float, travel_truck_time: float) -> None:
-        self.id_distance: uuid = id_distance
+        self.id_distance: uuid = str(uuid.uuid4())  # type: ignore
         self.id_city_origin: int = id_city_origin
         self.id_city_destiny: int = id_city_destiny
         self.minutes: float = minutes
